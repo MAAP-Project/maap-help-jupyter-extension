ae8e2cc29a6ab7d6f65723e1c898868b14f89da0
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2022-12-07 10:47:19 -0800

    converted code back to not trying the mamba way

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,6 @@
-import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
-import { MainAreaWidget } from '@jupyterlab/apputils';
-/**
- * Initialization data for the jupyterlab_kernel_companions extension.
- */
-declare const extensions: JupyterFrontEndPlugin<MainAreaWidget<import("@lumino/widgets").Widget>, JupyterFrontEnd.IShell, "desktop" | "mobile">[];
-export default extensions;
+/** jupyterlab imports **/
+import { JupyterFrontEndPlugin } from '@jupyterlab/application';
+/** internal imports **/
+import '../style/index.css';
+declare const extension: JupyterFrontEndPlugin<void>;
+export default extension;
--- src/index.ts
+++ src/index.ts
@@ -1,167 +1,103 @@
-import {
-    ILayoutRestorer,
-    JupyterFrontEnd,
-    JupyterFrontEndPlugin
-  } from '@jupyterlab/application';
-  import {
-    ICommandPalette,
-    MainAreaWidget,
-    WidgetTracker
-  } from '@jupyterlab/apputils';
-  import { IMainMenu } from '@jupyterlab/mainmenu';
-  import { ISettingRegistry } from '@jupyterlab/settingregistry';
-  import {
-    CondaEnvWidget
-  } from '@mamba-org/gator-common';
-  import { managerTour } from './tour';
-  
-  const CONDAENVID = '@mamba-org/gator-lab:plugin';
-  const TOUR_DELAY = 1000;
-  const TOUR_TIMEOUT = 5 * TOUR_DELAY + 1;
+/** jupyterlab imports **/
+import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application'; 
+import { ICommandPalette } from '@jupyterlab/apputils';
+import { PageConfig } from '@jupyterlab/coreutils'
+import { IMainMenu } from '@jupyterlab/mainmenu';
 
-  let condaWidget: MainAreaWidget<CondaEnvWidget>;
-  
-  
-  async function activateCondaEnv(
-    app: JupyterFrontEnd,
-    settingsRegistry: ISettingRegistry | null,
-    palette: ICommandPalette | null,
-    menu: IMainMenu | null,
-    restorer: ILayoutRestorer | null
-  ): Promise<MainAreaWidget> {
-    let tour: any;
-    const { commands, shell } = app;
-    const pluginNamespace = 'conda-env';
-    const command = 'jupyter_conda:open-ui';
-    console.log("in activate function");
-  
-    // Track and restore the widget state
-    const tracker = new WidgetTracker<MainAreaWidget<CondaEnvWidget>>({
-      namespace: pluginNamespace
-    });
-    
-    commands.addCommand(command, {
-      label: 'Conda Packages Manager',
-      execute: () => {
-        
-        console.log("checking if has command add");
-        //if (commands.hasCommand('jupyterlab-tour:add')) {
-            console.log("in if statement that has add command");
-            if (!tour) {
-                console.log("about to execute jupyter lab tour add");
-                
-              commands
-                .execute('jupyterlab-tour:add', {
-                  tour: managerTour as any
-                })
-                .then(result => {
-                  tour = result;
-                });
-            }
-          //}
-        
-        
-        app.restored.then(() => {
-          let timeout = 0;
-            console.log("in app restored then");
-          const delayTour = (): void => {
-            console.log("in creating delay tour function");
-            setTimeout(() => {
-              timeout += TOUR_DELAY;
-              console.log("above conda widget if statement launch");
-              if (condaWidget?.isVisible && tour) {
-                console.log("about to execute jupyter lab tour launch");
-                commands.execute('jupyterlab-tour:launch', {
-                  id: tour.id,
-                  force: false
-                });
-              } else if (timeout < TOUR_TIMEOUT) {
-                delayTour();
-              }
-            }, 1000);
-          };
-  
-          if (commands.hasCommand('jupyterlab-tour:add')) {
-            if (!tour) {
-                console.log("about to execute jupyter lab tour add");
-                
-              commands
-                .execute('jupyterlab-tour:add', {
-                  tour: managerTour as any
-                })
-                .then(result => {
-                  tour = result;
-                });
-            }
-  
-            delayTour();
-          }
-        });
-  
-        /*if (!condaWidget || condaWidget.isDisposed) {
-          condaWidget = new MainAreaWidget({
-            content: new CondaEnvWidget(model)
-          });
-          condaWidget.addClass(CONDA_WIDGET_CLASS);
-          condaWidget.id = pluginNamespace;
-          condaWidget.title.label = 'Packages';
-          condaWidget.title.caption = 'Conda Packages Manager';
-          condaWidget.title.icon = condaIcon;
-        }*/
-  
-        if (!tracker.has(condaWidget)) {
-          // Track the state of the widget for later restoration
-          tracker.add(condaWidget);
-        }
-        if (!condaWidget.isAttached) {
-          // Attach the widget to the main work area if it's not there
-          shell.add(condaWidget, 'main');
-        }
-        shell.activateById(condaWidget.id);
-      }
-    });
-  
-    // Add command to command palette
-    if (palette) {
-      palette.addItem({ command, category: 'Settings' });
-    }
-  
-    // Handle state restoration.
-    if (restorer) {
-      restorer.restore(tracker, {
-        command,
-        name: () => pluginNamespace
-      });
-    }
-  
-    // Add command to settings menu
-    if (menu) {
-      menu.settingsMenu.addGroup([{ command: command }], 999);
-    }
+import { ITourHandler } from "jupyterlab-tour";
 
-    return condaWidget;
-  
+/** phosphor imports **/
+import { Menu } from '@lumino/widgets';
+
+/** internal imports **/
+import '../style/index.css';
+//import { IFrameWidget } from './widgets';
+
+
+console.log(PageConfig.getBaseUrl());
+
+///////////////////////////////////////////////////////////////
+//
+// Earthdata Search Client extension
+//
+///////////////////////////////////////////////////////////////
+
+/*const extension: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab-apod',
+  autoStart: true,
+  requires: [ICommandPalette],
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
+    console.log('JupyterLab extension jupyterlab_apod is activated!');
+    console.log('ICommandPalette:', palette);
   }
-  
-  
-  
-  /**
-   * Initialization data for the @mamba-org/gator-lab extension.
-   */
-  const condaManager: JupyterFrontEndPlugin<MainAreaWidget> = {
-    id: CONDAENVID,
-    autoStart: true,
-    activate: activateCondaEnv,
-    optional: [ISettingRegistry, ICommandPalette, IMainMenu, ILayoutRestorer]
-    };
-  
-  /**
-   * Initialization data for the jupyterlab_kernel_companions extension.
-   */
-  
-  
-  const extensions = [condaManager];
-  
-  export default extensions;
-  
-  
+};
+
+export default extension;*/
+
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'maap-help',
+  autoStart: true,
+  optional: [ICommandPalette, IMainMenu],
+  // requires: [ICommandPalette, IStateDB],
+  activate: async (app: JupyterFrontEnd,
+    palette: ICommandPalette,
+    mainMenu: IMainMenu,) => {
+
+      const tour = (await app.commands.execute('jupyterlab-tour:add', {
+        tour: { // Tour must be of type ITour - see src/tokens.ts
+          id: 'test-jupyterlab-tour:welcome',
+          label: 'Welcome Tour',
+          hasHelpEntry: true,
+          steps: [  // Step must be of type IStep - see src/tokens.ts
+            {
+              content:
+                'The following tutorial will point out some of the main UI components within JupyterLab.',
+              placement: 'center',
+              target: '#jp-main-dock-panel',
+              title: 'Welcome to Jupyter Lab!'
+            },
+            {
+              content:
+                'This is the main content area where notebooks and other content can be viewed and edited.',
+              placement: 'left-end',
+              target: '#jp-main-dock-panel',
+              title: 'Main Content'
+            }
+          ],
+          // can also define `options`
+        }
+      })) as ITourHandler;
+      if ( tour ) {
+        app.commands.execute('jupyterlab-tour:launch', {
+          id: 'test-jupyterlab-tour:welcome',
+          force: false  // Optional, if false the tour will start only if the user have not seen or skipped it
+        })
+      }
+
+      const about_command = 'iframe:about';
+      app.commands.addCommand(about_command, {
+        label: 'About',
+        isEnabled: () => true,
+        execute: args => {
+          console.log("in execute of about");
+          //aboutPopup();
+        }
+      });
+      palette.addItem({command: about_command, category: 'Help'});
+  const { commands } = app;
+  let helpMenu = new Menu({ commands });
+  helpMenu.title.label = 'Help';
+  [
+    about_command
+  ].forEach(command => {
+    helpMenu.addItem({ command });
+  });
+  mainMenu.addMenu(helpMenu, { rank: 100 });
+
+
+  console.log('JupyterLab extension maap_help is activated!');
+  },
+};
+
+
+export default extension;
