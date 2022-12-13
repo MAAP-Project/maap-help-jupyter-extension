9195ef3307fe79ff34056bc89a2cf3baab6ee8f0
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2022-12-13 08:00:50 -0800

    converted back to using addItem

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@
 import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application'; 
 import { ICommandPalette } from '@jupyterlab/apputils';
 import { IMainMenu } from '@jupyterlab/mainmenu';
-import { ILauncher } from '@jupyterlab/launcher';
 
 /** internal imports **/
 import '../style/index.css';
@@ -82,7 +81,7 @@ const extension: JupyterFrontEndPlugin<void> = {
         }
     });
     palette.addItem({ command: maap_py_command, category: 'Help' });
-    /*[
+    [
         about_command,
         faq_command,
         tech_doc_command,
@@ -90,17 +89,7 @@ const extension: JupyterFrontEndPlugin<void> = {
         maap_py_command
     ].forEach(command => {
         mainMenu.helpMenu.addItem({ command });
-    });*/
-    var helpCommands = [
-      about_command,
-      faq_command,
-      tech_doc_command,
-      tutorials_command,
-      maap_py_command];
-    for (const helpCommand in helpCommands) {
-      console.log(helpCommand);
-      ILauncher.IItemOptions = { command:helpCommand };
-    }
+    });
     
 
     app.restored.then(() => {
@@ -115,7 +104,6 @@ const extension: JupyterFrontEndPlugin<void> = {
         sideBarElements = sideBarElements.filter(sideBarElement => determineIncludeSideBarElement(sideBarElement));
         sideBarElements.forEach(sideBarElement => sideBarElement.setAttribute('id', getSideBarId(sideBarElement)));
         sideBarElements.forEach(sideBarElement => console.log(sideBarElement.id));
-        
       }
     });
 
