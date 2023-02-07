ccbba30e7bef6f2cff07dcf62eac825ac605a162
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2023-02-07 07:26:40 -0800

    v42 fixed tsconfig to compile code and remove docmanager

--- src/plugin.tsx
+++ src/plugin.tsx
@@ -154,7 +154,7 @@ const extension: JupyterFrontEndPlugin<void> = {
       }
     });
 
-    console.log('JupyterLab extension maap_help v0.0.41 is activated!');
+    console.log('JupyterLab extension maap_help v0.0.42 is activated!');
   },
 };
 
