3130b267d450467687b94010ffdff178efedcabe
Marjorie Lucas <marjorie.j.lucas@jpl.nasa.gov>
2024-03-06 11:07:36 -0800

    python packaging | fixed build

--- src/__tests__/maap_help_jupyter_extension.spec.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Example of [Jest](https://jestjs.io/docs/getting-started) unit tests
- */
-
-describe('maap-help-jupyter-extension', () => {
-  it('should be tested', () => {
-    expect(1 + 1).toEqual(2);
-  });
-});
